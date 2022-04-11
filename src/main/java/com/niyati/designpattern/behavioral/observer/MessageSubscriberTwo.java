e9ed2734b571b9771c51abbdd4d079705a04bb6a
106540 <niyati.venugopal@thoughtfocous.com>
2022-04-11 12:06:13 +0530

    Changes have been updated.

--- src/main/java/com/niyati/designpattern/behavioral/observer/MessageSubscriberTwo.java
+++ src/main/java/com/niyati/designpattern/behavioral/observer/MessageSubscriberTwo.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MessageSubscriberTwo implements Observer{
-    private static Logger logger = LoggerFactory.getLogger(MessageSubscriberOne.class);
+    private static Logger logger = LoggerFactory.getLogger(MessageSubscriberTwo.class);
     @Override
     public void update(Message message) {
         logger.info("MessageSubscriberTwo:" + message.getMessageContent());
