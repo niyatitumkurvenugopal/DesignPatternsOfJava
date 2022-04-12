5a22aa96d9c4fa6f2ff37595e92b5272e4871f6f
106540 <niyati.venugopal@thoughtfocous.com>
2022-04-12 12:44:24 +0530

    Changes have been updated.

--- src/main/java/com/niyati/designpattern/creational/factory/MobileFactory.java
+++ src/main/java/com/niyati/designpattern/creational/factory/MobileFactory.java
@@ -6,10 +6,10 @@ import com.niyati.designpattern.creational.factory.OnePlus;
 
 public class MobileFactory {
 
-    public static Mobile getInstance(String type, int ram, String processor) {
-        if (type == "IPhone") {
+    public static Mobile getInstance(String str, int ram, String processor) {
+        if (str.equals("IPhone")) {
             return new IPhone(ram, processor);
-        } else if (type == "OnePlus") {
+        } else if (str.equals("OnePlus")) {
             return new OnePlus(ram, processor);
         }
         return null;
